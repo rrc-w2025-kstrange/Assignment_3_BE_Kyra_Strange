95e02830ec0732f41a88b9d52ec3c4eac70307d0
Kyra Strange <kstrangeguajardo@academic.rrc.ca>
2026-02-23 23:37:07 -0600

    Completed routes set up

--- src/api/v1/controllers/productController.ts
+++ src/api/v1/controllers/productController.ts
@@ -2,21 +2,31 @@ import { Request, Response } from "express";
 import { getAllItemsService } from "../services/productService";
 
 
-export const getAllItems = (req: Request, res: Response) => {
+export const getHealthCheck = (req: Request, res: Response) => {
     let result = getAllItemsService()
     res.status(200).json(result);
 };
 
-export const createItem = (req: Request, res: Response) => {
+export const getAllProducts = (req: Request, res: Response) => {
     let result = getAllItemsService()
     res.status(200).json(result);
 };
 
-export const updateItem = (req: Request, res: Response) => {
+export const getProductById = (req: Request, res: Response) => {
     let result = getAllItemsService()
     res.status(200).json(result);
 };
-export const deleteItem = (req: Request, res: Response) => {
+
+export const createProduct = (req: Request, res: Response) => {
+    let result = getAllItemsService()
+    res.status(200).json(result);
+};
+
+export const updateProduct = (req: Request, res: Response) => {
+    let result = getAllItemsService()
+    res.status(200).json(result);
+};
+export const deleteProduct = (req: Request, res: Response) => {
     let result = getAllItemsService()
     res.status(200).json(result);
 };
--- src/api/v1/routes/productRoutes.ts
+++ src/api/v1/routes/productRoutes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
-import { getItemById, createItem } from '../controllers/productController';
+import { getHealthCheck, getAllProducts, getProductById, createProduct, updateProduct, deleteProduct} from '../controllers/productController';
 
 const router: Router = Router();
 
-router.get('/:id', getItemById);
-router.post('/', createItem);
+router.get('/health', getHealthCheck);
+router.get('/products', getAllProducts);
+router.get('/products/:id', getProductById);
+router.post('/products', createProduct);
+router.put('/products/:id', updateProduct);
+router.delete('/products/:id', deleteProduct);
 
 export default router;
