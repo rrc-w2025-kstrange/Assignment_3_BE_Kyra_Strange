04d8126f561449f6756467af643da950c992dc70
Kyra Strange <kstrangeguajardo@academic.rrc.ca>
2026-02-24 18:25:49 -0600

    updated logic for createProductController

--- src/api/v1/controllers/productController.ts
+++ src/api/v1/controllers/productController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
-import { getHealthCheckService, getAllProductsService, getProductByIdService, createProductService, updateProductService, deleteProductService } from "../services/productService";
+import { getHealthCheckService, getAllProductsService, getProductByIdService, createProduct, updateProductService, deleteProductService } from "../services/productService";
+import { HTTP_STATUS } from "../../../constants/httpConstants";
 
 
 export const getHealthCheck = (req: Request, res: Response) => {
@@ -17,9 +18,17 @@ export const getProductById = (req: Request, res: Response) => {
     res.status(200).json(result);
 };
 
-export const createProduct = (req: Request, res: Response) => {
-    let result = createProductService("test")
-    res.status(200).json(result);
+export const createProductController = async (req: Request, res: Response) => {
+  try {
+    const product = await createProduct(req.body);
+    return res.status(HTTP_STATUS.CREATED).json({  
+      message: "Product created",
+      data: product,
+    });
+  } catch (err: unknown) {
+    const message = err instanceof Error ? err.message : "Unknown error";
+    return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({ message }); 
+  }
 };
 
 export const updateProduct = (req: Request, res: Response) => {
--- src/api/v1/routes/productRoutes.ts
+++ src/api/v1/routes/productRoutes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-import { getHealthCheck, getAllProducts, getProductById, createProduct, updateProduct, deleteProduct} from '../controllers/productController';
+import { getHealthCheck, getAllProducts, getProductById, createProductController, updateProduct, deleteProduct} from '../controllers/productController';
 
 const router: Router = Router();
 
 router.get('/health', getHealthCheck);
 router.get('/products', getAllProducts);
 router.get('/products/:id', getProductById);
-router.post('/products', createProduct);
+router.post('/products', createProductController);
 router.put('/products/:id', updateProduct);
 router.delete('/products/:id', deleteProduct);
 
