7607f533526e21ea470537e15bdbcdc7936f8785
Kyra Strange <kstrangeguajardo@academic.rrc.ca>
2026-03-01 13:15:28 -0600

    Updated error code for getEventById to match requirements (404)

--- src/api/v1/controllers/eventController.ts
+++ src/api/v1/controllers/eventController.ts
@@ -21,7 +21,7 @@ export const getEventById = async (req: Request, res: Response) => {
 
         res.status(HTTP_STATUS.OK).json(successResponse(results, "Event retrieved"));
     } catch (error) {
-        res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({ message: "Internal Server Error"});
+        res.status(HTTP_STATUS.NOT_FOUND).json({ message: "Internal Server Error"});
     }
 }
 
