09e7e5cbb342f1ae00dfb8a46c1aef78f4fd5987
Kyra Strange <kstrangeguajardo@academic.rrc.ca>
2026-03-01 16:30:53 -0600

    Configure Jest for TypeScript

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
-    setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"],
+    testMatch: ["**/*.test.ts"],
+    collectCoverageFrom: [
+        "src/**/*.ts",
+        "!src/server.ts", // Exclude server startup file
+        "!src/types/**/*.ts", // Exclude type definitions
+    ],
 };
